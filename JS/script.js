4f56a9af28f917ac005287b4f0710a02f7a69367
CristianN01 <cristiannechifor71@gmail.com>
2024-05-15 16:10:39 +0200

    rappresentato la lista sul html aggiungendo con un ciclo while gli elementi presenti in essa in una lista ul

---
+++ JS/script.js
@@ -0,0 +1,12 @@
+const shoppingList =['patate','cipolle','carote','carne','pasta','ruota'];
+const ulEl = document.querySelector('ul');
+
+elementList = 0;
+
+while (elementList < shoppingList.length) {
+    const element = document.createElement('li');
+    element.append(shoppingList[elementList]);
+    elementList++
+
+    ulEl.appendChild(element);
+}
